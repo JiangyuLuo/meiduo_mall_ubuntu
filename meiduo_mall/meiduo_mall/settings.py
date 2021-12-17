eb4d8e10bb6ae97412ff484cb21d2a590edc00fc
Jonathan <943215317@qq.com>
2021-12-17 08:20:37 +0800

    django-redis配置

--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -116,3 +116,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 配置redis数据库
+CACHES = {
+    "default": { # 预留
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "session": { # 用于保存session
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+}
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
+
